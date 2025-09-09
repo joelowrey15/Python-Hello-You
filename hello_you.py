33f1c495208f9994ad1028b7fd5040c7a2fdfa99
Joseph Lowrey <josephlowrey2007@gmail.com>
2025-09-09 21:55:06 +0000

    Print out greeting to name

--- hello_you.py
+++ hello_you.py
@@ -1,11 +1,14 @@
 """
-Explain what this program does
-First Last - Date
+This program ask a user for their name and says "Hello, name!"
+Joseph Lowrey - September 2025
 """
 
-
+# This is a single line comment
 def main() -> None:
-    pass
+    name: str="Joseph"
+    print("Hello,", name,"!")
+    print("Hello, "+ name + "!")
+    print(f"Hello, {name}!")
 
 
 if __name__ == "__main__":
